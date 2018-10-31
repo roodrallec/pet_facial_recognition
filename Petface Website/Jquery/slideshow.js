86470ff8140553d0ec7a44a64c0ccf8015ba2738
Jorge Alexander <jorge@maiv.co.uk>
2018-10-31 14:57:27 +0100

    fixed console errors; removed navbar; moved containers above slide images; removed google-maps for now

--- Petface Website/Jquery/slideshow.js
+++ Petface Website/Jquery/slideshow.js
@@ -9,6 +9,6 @@ function showSlides() {
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    if (slides[slideIndex-1]) slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
